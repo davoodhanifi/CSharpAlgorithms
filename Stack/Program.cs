38eef37dd1f014c1069eee0b5d0e6b3d7edfa2a7
davood.hanifi <davood.hanifi@gmail.com>
2022-01-13 22:44:03 +0330

    Created Stack.

---
+++ Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿using Stack;
+
+Stack.Stack theStack = new(4);
+theStack.Push("Star Wars");
+theStack.Push("Jaws");
+theStack.Push("Jaws 2");
+theStack.Push("Titanic");
+theStack.Push("Rocky");
+
+Console.WriteLine("================== \nAbout to peek:");
+Console.WriteLine($"'{ theStack.Peek() }'" + " is at the top of the stack.\n");
+
+Console.WriteLine("================== \nThe Stack contains:\n");
+while (!theStack.IsEmpty())
+{
+    string movie = theStack.Pop();
+    Console.WriteLine(movie);
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,55 @@
+﻿namespace Stack
+{
+    public class Stack
+    {
+        private int _maxSize;
+        private string[] _stackArray;
+        private int top;
+
+        public Stack(int maxSize)
+        {
+            this._maxSize = maxSize;
+            this._stackArray = new string[_maxSize];
+            this.top = -1;
+        }
+
+        public void Push(string item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is already full.");
+            }
+            else
+            {
+                _stackArray[++top] = item;
+            }
+        }
+
+        public string Pop()
+        {
+            if (IsEmpty())
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return _stackArray[top--];
+            }
+        }
+
+        public string Peek()
+        {
+            return _stackArray[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+          return _maxSize - 1 == top;
+        }
+    }
+}
