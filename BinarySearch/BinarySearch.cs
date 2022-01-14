e5aab8c76952d89b3718fa4e09256920b6fbe581
davood.hanifi <davood.hanifi@gmail.com>
2022-01-14 14:02:33 +0330

    Created Binary search.

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public int Search(int[] array, int item)
+        {
+            var firstPoint = 0;
+            var lastPoint = array.Length - 1;
+
+            while (firstPoint <= lastPoint)
+            {
+                var midPoint = (firstPoint + lastPoint) / 2;
+
+                if (item < array[midPoint])
+                    lastPoint = midPoint - 1;
+                else if (item > array[midPoint])
+                    firstPoint = midPoint + 1;
+                else
+                    return midPoint;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,9 @@
+﻿int theValue = 43;
+int[] array = new int[] { 11, 22, 43, 54, 57, 59, 62, 78 };
+BinarySearch.BinarySearch binarySearcher = new();
+
+Console.WriteLine("Our array contains: ");
+Array.ForEach(array, x => Console.Write(x + " "));
+
+Console.Write($"\n\nThe result of a binary search for {theValue} is: ");
+Console.WriteLine(binarySearcher.Search(array, theValue));
