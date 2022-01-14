4afa9ff123584a26a175142ec58225875b6ebc24
davood.hanifi <davood.hanifi@gmail.com>
2022-01-14 14:27:20 +0330

    Created Linear search.

---
+++ LinearSearch/LinearSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace LinearSearch
+{
+    public class LinearSearch
+    {
+        public int Search(int[] array, int item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == item)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,9 @@
+﻿var theValue = 7;
+int[] array = new[] { 1, 4, 5, 7, 10, 15, 20 };
+LinearSearch.LinearSearch linearSearcher = new();
+
+Console.WriteLine("Our array contains: ");
+Array.ForEach(array, x => Console.Write($"{x} "));
+
+Console.Write($"\n\nThe result of a binary search for {theValue} is: ");
+Console.WriteLine(linearSearcher.Search(array, theValue));
